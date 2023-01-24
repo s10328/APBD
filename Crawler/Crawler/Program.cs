9922e12c5e0159afc754153453b929b4f916dab5
Damian Czarnomorzec <s10328@pjwstk.edu.pl>
2023-01-24 20:03:57 +0100

    Working crawler

--- Crawler/Crawler/Program.cs
+++ Crawler/Crawler/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+namespace Crawler
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            string websiteUrl = args[0];
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync(websiteUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                var htmlContent = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9-]*@[a-z-]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                var emailAdress = regex.Matches(htmlContent);
+
+                foreach(var x in emailAdress)
+                {
+                    Console.WriteLine(x.ToString());   
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
